b576cfb50a1a4456602e7ef5d55abd5b02fbe0d0
Anna Radchenko <anrd.rs>
2023-01-27 17:47:33 +0300

    create app component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,9 +1,40 @@
-import s from "./index.module.css"
+import { useState } from "react";
+import { data } from "../../report-config";
+import { Form } from "../Form";
+import { Modal } from "../Modal";
+import { Table } from "../Table";
+import s from './index.module.css';
 
 export function App() {
-    return (
-      <div >
-        
+  const [user, setUser] = useState(data);
+  const [isModalOpen, setIsOpenModal] = useState(false);
+  const [modalData, setModalData] = useState();
+
+  const [inputValue, setInputValue] = useState({
+    name: '',
+    value: '',
+  });
+
+  const [editableUserData, setEditableUserData] = useState({
+    isEdit: false,
+    userIndex: null,
+  })
+
+  return (
+    <div className={s.wrapper}>
+    {isModalOpen && 
+        (
+          <Modal modalData={modalData} onClose={setIsOpenModal}>
+            <h1>Модальное окно</h1>
+          </Modal>
+        )
+      }
+      <div className={s.content}>
+        <Table />
+        <div className={s.formWrapper}>
+          <Form />
+        </div>
       </div>
-    );
-}  
+    </div>
+  );
+}
